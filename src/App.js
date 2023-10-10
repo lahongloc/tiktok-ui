fa311f318f66ef29d7b59ffb91a0559682313bc9
Hongloc2405 <hongloc111990@gmail.com>
2023-10-10 13:14:59 +0700

    configure using CSS/SCSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
     return (
         <div>
-            <a href="https://github.com/sondnpt00343/tiktok-ui/commit/b0f8e67a0a37d8d0c13216f0965485ea8794c663">
+            <h1>Cấu hình sử dụng CSS/SASS</h1>
+            <a href="https://github.com/sondnpt00343/tiktok-ui/commit/b24c39f9c5fa81374d0400db85ccddc62511b951">
                 Tutorial here
             </a>
         </div>
